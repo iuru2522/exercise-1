cbdf67f8ddb810750ff170b2e09ec74ea1f017d4
Iurii Manastyrskyi <manastyr@sheridancollege.ca>
2023-05-26 10:55:49 -0400

    added "I'm done" to the printInfo method

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -48,7 +48,7 @@ public class CardTrick {
      * @author Iurii Manastyrskyi May 2023
      */
     private static void printInfo() {
-    
+        //I'm done
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
